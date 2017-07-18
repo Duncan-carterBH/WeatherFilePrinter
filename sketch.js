bc7298921c6d678c27d0d86a6f5be9c2ae0da814
Duncan Carter <duncan.carter@burohappold.com>
2017-07-18 17:19:21 +0100

    Wiped all to start fresh

--- sketch.js
+++ sketch.js
@@ -1,13 +1,7 @@
 function setup() {
-createCanvas(720,400);
-background(200);
+
 }
 
 function draw() {
-  background(127);
-  // Draw a circle
-  strokeWeight(2);
-  stroke(0,0,0);
-  fill(50,50,50);
-  ellipse(360, 200, 200, 200);
+  
 }
